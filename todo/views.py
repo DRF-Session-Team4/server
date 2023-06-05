38f27bd287e1bd43ea0afff7cb71b420849fa8c0
kyubin0314@naver.com <kyubin0314@naver.com>
2023-06-05 20:04:30 +0900

    요구사항1 해결

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.viewsets import ModelViewSet
+from .serializers import TodoSerializer
+from .models import Todo
+class TodoViewSet(ModelViewSet):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
